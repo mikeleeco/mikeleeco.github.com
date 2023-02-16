d5095bde404b180649bb8ed0b3d9b32ffa9aca69
michael <mdlee12@gmail.com>
2023-02-16 14:51:49 -0500

    change adapter to netlify; remove link in to highlight.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-netlify";
 // shifted to static for use on github pages https://kit.svelte.dev/docs/adapters
+// shifted to netlify after fail https://github.com/mikeleeco/mikeleeco.github.com/pull/5
 // import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from "mdsvex";
 import { mdsvexConfig } from "./mdsvex.config.js";
